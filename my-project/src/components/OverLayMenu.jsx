b8772c3511f6265a04e9a02213a9f5834c69ed84
kamranahmed288390 <150603111+kamranahmed288390@users.noreply.github.com>
2026-02-01 15:01:11 -0500

    Day 4

--- my-project/src/components/OverLayMenu.jsx
+++ my-project/src/components/OverLayMenu.jsx
@@ -2,10 +2,20 @@ import { motion, AnimatePresence } from "framer-motion";
 import { FiX } from "react-icons/fi";
 
 export default function OverLayMenu({ isOpen, onClose }) {
+  const isMobile = typeof window !== "undefined" && window.innerWidth <1024;
+  const origin = isMobile ? "95% 8%" : "50% 8%"
+
+
   return (
     <AnimatePresence>
       {isOpen && (
-        <motion.div className="fixed inset-0 flex items-center justify-center z-50">
+        <motion.div className="fixed inset-0 flex items-center justify-center z-50"
+        initial = {{clipPath: `circle(0% at ${origin})`}}
+        animate = {{clipPath:   `circle(150% at ${origin})`}}
+        exit= {{clipPath: `circle(0% at ${origin})`}}
+        transition={{duration: 0.7, ease: [0.4,0,0.2,1]}}
+        style={{backgroundColor: "rgba(0,0,0,0.95)"}}
+        >
           <button
             onClick={onClose}
             className="absolute top-6 right-6 text-white text-3xl"
@@ -29,7 +39,10 @@ export default function OverLayMenu({ isOpen, onClose }) {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: 0.3 + index * 0.1 }}
               >
-                <a href={`#${item.toLowerCase()}`}>{item}</a>
+                <a href={`#${item.toLowerCase()}`}
+                onClick={onClose}
+                className="text-4xl text-white font-semibold hover:text-pink-400 transition-colors duartion-300"
+                >{item}</a>
               </motion.li>
             ))}
           </ul>
